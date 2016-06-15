5631ba509d7e015bf9ccc8a5729b8ce217acab08
Ellimyst <sumukhvaze@gmail.com>
2016-06-15 15:48:49 +0530

    arraylist tree

--- Runner.java
+++ Runner.java
@@ -1,15 +1,12 @@
+import java.util.ArrayList;
 
 public class Runner {
 
-	static String a; 
-	
-	public Runner() {
-		a = "git";
-	}
-
 	public static void main(String[] args) {
-		System.out.print(a);
-
+		
+		ArrayList<Tree> trees = new ArrayList<Tree>();
+		
+		
 	}
 
 }
